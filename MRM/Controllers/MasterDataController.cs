d3d2153f431e324d119688a2f9635e73f8fb97fe
Faizan Ahmad <Shailesh.dwivedi@in.hanusoftware.com>
2017-06-07 12:16:09 +0530

    Add master data form

---
+++ MRM/Controllers/MasterDataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MRM.Controllers
+{
+    public class MasterDataController : Controller
+    {
+        // GET: MasterData
+        public ActionResult MasterData()
+        {
+            return View();
+        }
+    }
+}
