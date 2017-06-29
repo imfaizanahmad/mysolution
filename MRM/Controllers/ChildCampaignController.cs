346bd65d9576a0bc67d9e9edb8da237f1c26792d
shailesh.dwivedi <shailesh.dwivedi@HANU-420>
2017-06-29 20:48:03 +0530

    fixed issues

--- MRM/Controllers/ChildCampaignController.cs
+++ MRM/Controllers/ChildCampaignController.cs
@@ -268,6 +268,8 @@ namespace MRM.Controllers
                 if (model.BusinessGroups_Id == null) errorCounter++;
                 if (model.BusinessLines_Id == null) errorCounter++;
                 if (model.Segments_Id == null) errorCounter++;
+                if (model.StartDate == null) errorCounter++;
+                if (model.EndDate == null) errorCounter++;
                 //if (model.CampaignTypes == CampaignType.GEPS && model.Segments_Id == null) errorCounter++;
                 if (Convert.ToDateTime(model.StartDate) > Convert.ToDateTime(model.EndDate)) errorCounter++;
                 if (model.Name == "") errorCounter++;
