d3d9f4617862c57ae3db758c87fcaa8161fdf8c5
Faizan Ahmad <Shailesh.dwivedi@in.hanusoftware.com>
2017-07-17 12:16:57 +0530

    Add journey stage class

---
+++ MRM.Database/Model/JourneyStage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRM.Database.Model
+{
+    public class JourneyStage : CommonEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
